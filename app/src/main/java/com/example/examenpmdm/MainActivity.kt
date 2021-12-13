499d55935e8a5915475bfb3e14a1b9af3ffff2e8
aalvarezbretana <aalvarezbretana@danielcastelao.org>
2021-12-13 10:45:52 +0100

    Toast al pinchar en el botón

---
+++ app/src/main/java/com/example/examenpmdm/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.examenpmdm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val boton = findViewById<Button>(R.id.button)
+        val toast = Toast.makeText(applicationContext, R.string.mensaje, Toast.LENGTH_SHORT)
+
+        boton.setOnClickListener{
+            toast.show()
+
+        }
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("Estado","onResume")
+    }
+
+
+}
